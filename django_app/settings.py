8ebd30a9de6093a419c07c3b9374a9474ad7b492
hanifaudah <hanifarkanaudah@gmail.com>
2020-02-25 22:34:00 +0700

    Fixed settings

--- django_app/settings.py
+++ django_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n(5x-2$3fobyxtmox6+q*widcim&(hv^+4*%5(__wkd3f=6rbw'
+# SECRET_KEY = 'n(5x-2$3fobyxtmox6+q*widcim&(hv^+4*%5(__wkd3f=6rbw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
